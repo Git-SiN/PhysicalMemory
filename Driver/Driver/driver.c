6113da0bc8138d1cbcee2e8417133327502adb61
신동하 <miceshin@naver.com>
2017-11-03 21:53:53 +0900

    Initialize code for driver...

---
+++ Driver/Driver/driver.c
@@ -0,0 +1,29 @@
+#include "ntddk.h"
+
+
+
+VOID DriverUnload(PDRIVER_OBJECT pDriverObject)
+{
+	DbgPrintEx(101, 0, "Driver Unloaded...\n");
+
+}
+
+NTSTATUS DispatchRoutine(PDEVICE_OBJECT pDeviceObject, PIRP pIrp) {
+	pIrp->IoStatus.Status = STATUS_SUCCESS;
+	pIrp->IoStatus.Information = 0;
+
+	IoCompleteRequest(pIrp, IO_NO_INCREMENT);
+	return STATUS_SUCCESS;
+
+}
+
+NTSTATUS DriverEntry(PDRIVER_OBJECT pDriverObject, PUNICODE_STRING regPath) {
+	ULONG i = 0;
+	
+	for (i = 0; i < IRP_MJ_MAXIMUM_FUNCTION; i++)
+		pDriverObject->MajorFunction[i] = DispatchRoutine;
+
+	pDriverObject->DriverUnload = DriverUnload;
+
+	return STATUS_SUCCESS;
+}
