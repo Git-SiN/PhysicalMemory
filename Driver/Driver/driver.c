33cd97d28c89e8e4ad9e31095e792f592f9755a9
신동하 <miceshin@naver.com>
2017-11-07 00:56:55 +0900

    Separate Routines for manipulaion & Restore

--- Driver/Driver/driver.c
+++ Driver/Driver/driver.c
@@ -3,12 +3,19 @@
 //////////////////////////////////////////////////////////////////////////////////
 //////////////////////		HARDCODING for test		//////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////
-#define MEMORY_SIZE					4				// GB
-#define TARGET_EPROCESS					0x8682cd28        		// for test
+#define MEMORY_SIZE						4							// GB
+#define TARGET_EPROCESS					0x85a60030              		// for test
 //////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////
 
+typedef struct _BACKUP_INFORMATION {
+	ULONG BackedEprocess;
+	ULONG BackedCR3;
+	ULONG BackedEthread;
+} BACKUP_INFORMATION, *PBACKUP_INFORMATION;
+
+PBACKUP_INFORMATION pBackup = NULL;
 
 VOID DriverUnload(PDRIVER_OBJECT pDriverObject)
 {
@@ -24,60 +31,34 @@ NTSTATUS DispatchRoutine(PDEVICE_OBJECT pDeviceObject, PIRP pIrp) {
 		
 }
 
-
-VOID DumpPhysicalMemory(ULONG CountOfPageFrame) {
-	PHYSICAL_ADDRESS pa;
-	ULONG i = 0;
-	ULONG virtualAddress = 0;
-	PULONG myAddress = NULL;
-	PULONG targetAddress = NULL;
+NTSTATUS ManipulateForSniffing(ULONG targetEprocess) {
+	ULONG targetPDT = 0;
 	ULONG backedEprocess = 0;
 	ULONG backedCR3 = 0;
 	ULONG backedEthread = 0;
-	ULONG targetPDT = 0;
 
-
-	if (TARGET_EPROCESS >= 0x80000000) {
+	if ((pBackup == NULL) && (targetEprocess >= 0x80000000) && (*(PUCHAR)(targetEprocess - 0xC) == 0x7)) {		
 		__try {
-			targetPDT = *(PULONG)(TARGET_EPROCESS + KPROC_OFFSET_DirectoryTableBase);
+			targetPDT = *(PULONG)(targetEprocess + KPROC_OFFSET_DirectoryTableBase);
 		}
 		__except (EXCEPTION_EXECUTE_HANDLER) {
-			DbgPrintEx(101, 0, "[ERROR] TARGET_ERPOCESS is Invalid address...\n");
-			return;
+			DbgPrintEx(101, 0, "[ERROR] Can't find the PDT...\n");
+			return STATUS_UNSUCCESSFUL;
 		}
 	}
-	else
-		return;
-	
-	myAddress = ExAllocatePool(NonPagedPool, CountOfPageFrame * 4);
-	if (myAddress == NULL)
-		return;
-
-	targetAddress = ExAllocatePool(NonPagedPool, CountOfPageFrame * 4);
-	if (targetAddress == NULL) {
-		ExFreePool(myAddress);
-		return;
+	else {
+		DbgPrintEx(101, 0, "[ERROR] Invalid EPROCESS...\n");
+		return STATUS_UNSUCCESSFUL;
 	}
-
-	RtlZeroMemory(myAddress, CountOfPageFrame * 4);
-	RtlZeroMemory(targetAddress, CountOfPageFrame * 4);
-	////////////////////////////////////////////////////////////////////////////////////////
-
-
-	for (i = 0; i < CountOfPageFrame; i++) {
-		pa.QuadPart = (i << 12);
-		__try {
-			virtualAddress = (ULONG)MmGetVirtualForPhysical(pa);
-			//	DbgPrintEx(101, 0, "PA : [%06X000]     at 0x%08X\n", i, virtualAddress);
-			myAddress[i] = virtualAddress;
-		}
-		__except (EXCEPTION_EXECUTE_HANDLER) {
-			DbgPrintEx(101, 0, "PA : [%06X000]       Exception occured...\n", i);
-		}
+		
+	pBackup = ExAllocatePool(NonPagedPool, sizeof(BACKUP_INFORMATION));
+	if (pBackup == NULL) {
+		DbgPrintEx(101, 0, "[ERROR] Failed to AllocatePool...\n");
+		return STATUS_UNSUCCESSFUL;
 	}
+	RtlZeroMemory(pBackup, sizeof(BACKUP_INFORMATION));
 
-
-	// Backup & Switching 
+	// Backup & Manipulation 
 	__try {
 		__asm {
 			push eax;
@@ -92,12 +73,12 @@ VOID DumpPhysicalMemory(ULONG CountOfPageFrame) {
 
 			add eax, KTHREAD_OFFSET_KPROCESS;
 			mov ebx, [eax];						// current Thread's EPROCESS
-			mov backedEprocess, ebx;		
+			mov backedEprocess, ebx;
 
 
-			/////////////////////		Switching		/////////////////////
-			mov dword ptr [eax], TARGET_EPROCESS;
-			
+			/////////////////////		Manipulation		/////////////////////
+			mov dword ptr ds:[eax], TARGET_EPROCESS;
+
 			mov ebx, targetPDT;
 			mov cr3, ebx;
 
@@ -105,28 +86,38 @@ VOID DumpPhysicalMemory(ULONG CountOfPageFrame) {
 			pop eax;
 
 		}
-		//*(PULONG)(backedEthread + KTHREAD_OFFSET_KPROCESS) = TARGET_EPROCESS;
+
 	}
 	__except (EXCEPTION_EXECUTE_HANDLER) {
-		DbgPrintEx(101, 0, "[ERROR] Failed to switch...\n");
-		goto PRINT;
+		DbgPrintEx(101, 0, "[ERROR] Failed to manipulate...\n");
+		ExFreePool(pBackup);
+		pBackup = NULL;
+		return STATUS_UNSUCCESSFUL;
 	}
 
-//	DbgPrintEx(101, 0, "EPROCESS : %s[0x%08X]    CR3 : 0x%08X\n", (PUCHAR)(backedEprocess + EPROC_OFFSET_ImageFileName), backedEprocess, backedCR3);
+	pBackup->BackedEprocess = backedEprocess;
+	pBackup->BackedCR3 = backedCR3;
+	pBackup->BackedEthread = backedEthread;
+	
+	DbgPrintEx(101, 0, "Succeeded to manipulate...\n");
+	return STATUS_SUCCESS;
+}
 
-	for (i = 0; i < CountOfPageFrame; i++) {
-		pa.QuadPart = (i << 12);
-		__try {
-			virtualAddress = (ULONG)MmGetVirtualForPhysical(pa);
-			//	DbgPrintEx(101, 0, "PA : [%06X000]     at 0x%08X\n", i, virtualAddress);
-			targetAddress[i] = virtualAddress;
-		}
-		__except (EXCEPTION_EXECUTE_HANDLER) {
-			DbgPrintEx(101, 0, "PA : [%06X000]       Exception occured...\n", i);
-		}
+
+VOID RestoreManipulation()
+{
+	ULONG backedCR3 = 0;
+	ULONG backedEthread = 0;
+
+	if (pBackup == NULL) {
+		DbgPrintEx(101, 0, "[ERROR] Not Manipulated...\n");
+		return;
 	}
 
-	// RESTORE...
+	backedCR3 = pBackup->BackedCR3;
+	backedEthread = pBackup->BackedEthread;
+	
+	// RESTORE CR3...
 	__try {
 		__asm {
 			push eax;
@@ -149,12 +140,75 @@ VOID DumpPhysicalMemory(ULONG CountOfPageFrame) {
 		}
 	}
 	__except (EXCEPTION_EXECUTE_HANDLER) {
-		DbgPrintEx(101, 0, "[ERROR] in RESTORE CR3...\n");
+		DbgPrintEx(101, 0, "[ERROR] Failed to restore CR3...\n");
 	}
 
-	*(PULONG)(backedEthread + KTHREAD_OFFSET_KPROCESS) = backedEprocess;
+	// RESTORE EPROCESS in ETHREAD
+	*(PULONG)(backedEthread + KTHREAD_OFFSET_KPROCESS) = pBackup->BackedEprocess;
 
-PRINT:
+	ExFreePool(pBackup);
+	pBackup = NULL;
+
+	DbgPrintEx(101, 0, "Succeeded to restore...\n");
+	return;
+}
+
+VOID DumpPhysicalMemory(ULONG targetEprocess, ULONG CountOfPageFrame) {
+	PHYSICAL_ADDRESS pa;
+	ULONG i = 0;
+	ULONG virtualAddress = 0;
+	PULONG myAddress = NULL;
+	PULONG targetAddress = NULL;
+
+
+	myAddress = ExAllocatePool(NonPagedPool, CountOfPageFrame * 4);
+	if (myAddress == NULL)
+		return;
+
+	targetAddress = ExAllocatePool(NonPagedPool, CountOfPageFrame * 4);
+	if (targetAddress == NULL) {
+		ExFreePool(myAddress);
+		return;
+	}
+
+	RtlZeroMemory(myAddress, CountOfPageFrame * 4);
+	RtlZeroMemory(targetAddress, CountOfPageFrame * 4);
+	////////////////////////////////////////////////////////////////////////////////////////
+
+	for (i = 0; i < CountOfPageFrame; i++) {
+		pa.QuadPart = (i << 12);
+		__try {
+			virtualAddress = (ULONG)MmGetVirtualForPhysical(pa);
+			//	DbgPrintEx(101, 0, "PA : [%06X000]     at 0x%08X\n", i, virtualAddress);
+			myAddress[i] = virtualAddress;
+		}
+		__except (EXCEPTION_EXECUTE_HANDLER) {
+			DbgPrintEx(101, 0, "PA : [%06X000]       Exception occured...\n", i);
+		}
+	}
+
+	// Examine at the target side...
+	if (!NT_SUCCESS(ManipulateForSniffing(targetEprocess))) {
+		ExFreePool(myAddress);
+		ExFreePool(targetAddress);
+		return;
+	}
+	
+	for (i = 0; i < CountOfPageFrame; i++) {
+		pa.QuadPart = (i << 12);
+		__try {
+			virtualAddress = (ULONG)MmGetVirtualForPhysical(pa);
+			//	DbgPrintEx(101, 0, "PA : [%06X000]     at 0x%08X\n", i, virtualAddress);
+			targetAddress[i] = virtualAddress;
+		}
+		__except (EXCEPTION_EXECUTE_HANDLER) {
+			DbgPrintEx(101, 0, "PA : [%06X000]       Exception occured...\n", i);
+		}
+	}
+
+	RestoreManipulation();
+	
+	// Output...
 	for (i = 0; i < CountOfPageFrame; i++) {
 		DbgPrintEx(101, 0, "PA : [%06X000]     %08X     %08X\n", i, myAddress[i], targetAddress[i]);
 	}
@@ -176,7 +230,7 @@ NTSTATUS DriverEntry(PDRIVER_OBJECT pDriverObject, PUNICODE_STRING regPath) {
 
 	pDriverObject->DriverUnload = DriverUnload;
 	
-	DumpPhysicalMemory(MEMORY_SIZE * 256);	// GB to Pages
+	DumpPhysicalMemory(TARGET_EPROCESS, MEMORY_SIZE * 256);	// GB to Pages
 	
 	DbgPrintEx(101, 0, "Driver loaded...\n");
 	return STATUS_SUCCESS;
--- Headers/driver.h
+++ Headers/driver.h
@@ -5,3 +5,107 @@
 #define KTHREAD_OFFSET_KPROCESS						0x150		//  Ptr32 _KPROCESS
 #define KPROC_OFFSET_DirectoryTableBase				0x018		//  Uint4B
 #define EPROC_OFFSET_VadRoot						0x278		//  _MM_AVL_TABLE
+
+
+#pragma pack(1)
+/////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////			MMWSL			/////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////
+typedef struct _MMWSLE {
+	union {
+		PVOID VirtualAddress;   // PVoid
+		LONG Long;
+		struct _MMWSLENTRY {
+			ULONG Valid : 1;
+			ULONG Spare : 1;
+			ULONG Hashed : 1;
+			ULONG Direct : 1;
+			ULONG Protection : 5;
+			ULONG Age : 3;
+			ULONG VirtualPageNumber : 20;
+		}e1;
+		struct _MMWSLE_FREE_ENTRY {
+			ULONG MustBeZero : 1;
+			ULONG PreviousFree : 11;
+			ULONG NextFree : 20;
+		}e2;
+	}u1;
+} MMWSLE, *PMMWSLE;
+
+typedef struct _MMWSLE_NONDIRECT_HASH {
+	PVOID Key;             // Ptr32 Void
+	ULONG Index;            //  Uint4B
+} MMWSLE_NONDIRECT_HASH, *PMMWSLE_NONDIRECT_HASH;
+
+typedef struct _MMWSLE_HASH {
+	ULONG Index; //Uint4B
+}MMWSLE_HASH, *PMMWSLE_HASH;
+
+typedef struct _MMWSL {
+	ULONG FirstFree;  //Uint4B
+	ULONG  FirstDynamic;  //Uint4B
+	ULONG LastEntry;  //Uint4B
+	ULONG  NextSlot;  //Uint4B
+	PMMWSLE	Wsle;  //Ptr32 _MMWSLE
+	PVOID LowestPagableAddress;  //Ptr32 Void
+	ULONG  LastInitializedWsle;  //Uint4B
+	ULONG  NextAgingSlot;  //Uint4B
+	ULONG  NumberOfCommittedPageTables;  //Uint4B
+	ULONG VadBitMapHint;  //Uint4B
+	ULONG  NonDirectCount;  //Uint4B
+	ULONG  LastVadBit;  //Uint4B
+	ULONG  MaximumLastVadBit;  //Uint4B
+	ULONG  LastAllocationSizeHint;  //Uint4B
+	ULONG LastAllocationSize;  //Uint4B
+	PMMWSLE_NONDIRECT_HASH NonDirectHash;  //Ptr32 _MMWSLE_NONDIRECT_HASH
+	PMMWSLE_HASH HashTableStart;  //Ptr32 _MMWSLE_HASH
+	PMMWSLE_HASH HighestPermittedHashAddress;  //Ptr32 _MMWSLE_HASH
+	USHORT UsedPageTableEntries[1536];  //[1536] Uint2B
+	ULONG CommittedPageTables[48];  //[48] Uint4B
+} MMWSL, *PMMWSL;
+
+typedef struct _MMSUPPORT {
+	ULONG WorkingSetMutex; // _EX_PUSH_LOCK
+	PVOID ExitGate; // Ptr32 _KGATE
+	PVOID AccessLog; // Ptr32 Void
+	LIST_ENTRY WorkingSetExpansionLinks; // _LIST_ENTRY
+	ULONG AgeDistribution[7]; // [7] Uint4B
+	ULONG MinimumWorkingSetSize; // Uint4B
+	ULONG WorkingSetSize; // Uint4B
+	ULONG  WorkingSetPrivateSize; // Uint4B
+	ULONG  MaximumWorkingSetSize; // Uint4B
+	ULONG  ChargedWslePages; // Uint4B
+	ULONG  ActualWslePages; // Uint4B
+	ULONG  WorkingSetSizeOverhead; // Uint4B
+	ULONG  PeakWorkingSetSize; // Uint4B
+	ULONG  HardFaultCount; // Uint4B
+	PMMWSL VmWorkingSetList; // Ptr32 _MMWSL
+	USHORT NextPageColor; // Uint2B
+	USHORT LastTrimStamp; // Uint2B
+	ULONG  PageFaultCount; // Uint4B
+	ULONG  RepurposeCount; // Uint4B
+	ULONG Spare[1]; // [1] Uint4B
+	struct _MMSUPPORT_FLAGS {
+		ULONG WorkingSetType : 3;
+		ULONG ModwriterAttached : 1;
+		ULONG TrimHard : 1;
+		ULONG MaximumWorkingSetHard5 : 1;
+		ULONG ForceTrim : 1;
+		ULONG MinimumWorkingSetHard7 : 1;
+		ULONG SessionMaster : 1;
+		ULONG TrimmerState : 2;
+		ULONG Reserved : 1;
+		ULONG PageStealers : 4;
+		ULONG MemoryPriority : 8;
+		ULONG WsleDeleted : 1;
+		ULONG VmExiting : 1;
+		ULONG ExpansionFailed : 1;
+		ULONG Available : 5;
+	} Flags;
+}MMSUPPORT, *PMMSUPPORT;
+/////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+
+#pragma pack()
